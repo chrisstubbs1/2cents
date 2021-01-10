a25f50c2a3dd9761a20e7b770d9fc54c7a4db569
Chris Stubbs <stubbs.chris2@gmail.com>
2021-01-10 13:24:38 -0500

    reconfigured project, react-native run-android now works

--- android/app/src/main/java/com/twocents/MainActivity.java
+++ android/app/src/main/java/com/twocents/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testapp;
+package com.twocents;
 
 import com.facebook.react.ReactActivity;
 
@@ -6,10 +6,10 @@ public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * rendering of  the component.
    */
   @Override
   protected String getMainComponentName() {
-    return "twocents";
+    return "main";
   }
 }
